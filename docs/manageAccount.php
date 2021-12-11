8a999c743ca7a1fec51b953ded88971809529144
LeslieTepale <77855205+LeslieTepale@users.noreply.github.com>
2021-12-10 22:40:12 -0500

    signIn page fix

--- docs/manageAccount.php
+++ docs/manageAccount.php
@@ -1,48 +1,30 @@
 <?php
-
 require_once "db.php";
 
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
+if(isset($_POST['username'])){
+    //validating username input
+    if (!preg_match('/[\'^$%&*()}{@#~?<>,|=_+¬-]/', $_POST['username'])||
+        strpos($_POST['username'],'..') != false ||
+        strpos($_POST['username'], '.') == 0 ||
+        strpos($_POST['username'], '.') == strlen($_POST['username'])-1 || $_POST['username'] == "leslie") {
+        echo "Your New Username is invalid. Please Enter a Username that meets the above requirements";
+    }
+    else{
+        echo "Your username has been changed";
+    }
 }
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+if(isset($_POST['password']) && isset($_POST['new'])) {
+    //validating password input
+    if ($password != 'certain password') {
+        echo "The Password you entered is incorrect, please enter your current password";
+    }
+    if (strlen($_POST['new']) < 8 || strlen($_POST['new']) > 30
+        || preg_match('/0123456789/', $_POST['new']) == false) {
+        echo "Your New Password is invalid. Please enter a password that meets the above requirements";
+    }
 }
-
-
-$UNchange = $_REQUEST['username'];
-while (preg_match('/[\'^$%&*()}{@#~?<>,|=_+¬-]/', $UNchange)!=false ||
-    strpos($UNchange, '.') == 0 ||
-    strpos($UNchange, '.') == strlen($UNchange)-1 || $UNchange == "leslie") {
-    break;
-    echo "Your New Username is invalid. Please Enter a Username that meets the above requirements";
-
-}
-$password = $_REQUEST['password'];
-$newPW = $_REQUEST['new'];
-while($password!='certain password')
-{
-    break;
-    echo "The Password you entered is incorrect, please enter your current password";
-}
-while(strlen($newPW) < 8 || strlen($newPW) > 30
-    || preg_match('0123456789',$newPW) == false)
-{
-break;
-    echo "Your New Password is invalid. Please enter a password that meets the above requirements";
-}
-
-if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
+/*
+if (isset($_POST['username']) && isset($_POST['password'])) {
     if ($_REQUEST['username'] == 'admin' && $_REQUEST['password'] == 'admin') {
         return 'login done';
     }
@@ -50,4 +32,5 @@ if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
         return 'invalid user';
     }
 }
+*/
 ?>
